19a1dea72e14ed193c0d9a8ba827ce6c617e2ea8
Nikita-St3n <kuznetsov03n@gmail.com>
2025-09-07 21:01:34 +0400

    Full site + adaptive

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ const menuList = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('active');
 });
+
+const swiper = new Swiper('.projects__slider', {
+    loop: true,
+    spaceBetween: 20,
+    
+    breakpoints: {
+        640: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    },
+
+    navigation: {
+        nextEl: '.projects__arrow-next',
+        prevEl: '.projects__arrow-prev',
+    }
+});
